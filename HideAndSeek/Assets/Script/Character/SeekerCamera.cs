52d86f9f4dc90f9023518294be143a5b906bf866
足利 慶次 <a.keiji14@gmail.com>
2024-06-25 16:02:52 +0900

    鬼側の移動処理を追加

--- HideAndSeek/Assets/Script/Character/SeekerCamera.cs
+++ HideAndSeek/Assets/Script/Character/SeekerCamera.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SeekerCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region PrivateField
+    private float xRotation = 0f;
+    #endregion
+
+    #region SerializeField
+    /// <summary>Å‘åƒJƒƒ‰‹——£</summary>
+    [SerializeField] private float mouseSensitivity = 100.0f;
+    /// <summary>Å¬ƒJƒƒ‰‹——£</summary>
+    [SerializeField] private Transform playerBody;
+    #endregion
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        LookAround();
+    }
+
+    void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
--- HideAndSeek/Assets/Script/Character/SeekerController.cs
+++ HideAndSeek/Assets/Script/Character/SeekerController.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SeekerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region SerializeField
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpHeight = 2.0f;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private Transform cameraTransform;
+    #endregion
+
+    #region PrivateField
+    private bool isGrounded;
+    private Vector3 velocity;
+    private CharacterController characterController;
+    #endregion
+
+    #region UnityEvent
+    private void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
     }
+    #endregion
+
+    #region PrivateMethod
+    private void Move()
+    {
+        isGrounded = characterController.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+    #endregion
 }
